40797d6bb7eaf438376f93fdb75019ab073445c5
addyshine400 <adityaraj6374@gmail.com>
2026-07-19 13:41:06 +0530

    my second commit

--- invertedNumberTriangle.java
+++ invertedNumberTriangle.java
@@ -208,7 +208,7 @@
    // }
  
 
-    // inverted number triangle
+    //4> inverted number triangle
 
    //  public class invertedNumberTriangle{
    //    public static void main(String[] args) {
@@ -221,5 +221,37 @@
    //       }
    //    }
    //  }
+
+
+   // 5>incremented  number diamond
+
+//    public class  incrementNumberDiamond {
+//     public static void main(String[] args) {
+
+//         int n = 5;
+
+//         // Upper Half
+//         for (int i = 1; i <= n; i++) {
+
+//             // Print number i+2, i times
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print((i + 2) + " ");
+//             }
+
+//             System.out.println();
+//         }
+
+//         // Lower Half
+//         for (int i = n - 1; i >= 1; i--) {
+
+//             // Print number i+2, i times
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print((i + 2) + " ");
+//             }
+
+//             System.out.println();
+//         }
+//     }
+// }
 
    
